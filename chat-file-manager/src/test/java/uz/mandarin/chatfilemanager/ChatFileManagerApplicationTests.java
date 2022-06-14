2c0ccb66fc5ca08fb47fb89cc4e5edbacb6b0643
abduvohid0131@gmail.com <imueaa0131>
2022-06-14 23:01:31 +0500

    another modules

---
+++ chat-file-manager/src/test/java/uz/mandarin/chatfilemanager/ChatFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mandarin.chatfilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatFileManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
